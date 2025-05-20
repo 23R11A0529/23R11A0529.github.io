d090a862a63fffd9ab0f588c29e886e1cefb3200
23R11A0529 <23r11a0529@gcet.edu.in>
2025-05-20 18:45:40 +0530

    updated

--- p3/index.js
+++ p3/index.js
@@ -1,6 +1,6 @@
 const customers = [
     {card: "1234567890", pin:"1234", name:"John", balance:0},
-    {card: "1234567891", pin:"1235", name:"Cathy", balance:0},
+    {card: "1234567891", pin:"2345", name:"Cathy", balance:0},
     
 ];
 
