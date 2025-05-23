d74bbcc705b2d4e1013f3c6ff039808c0d8c29dc
23R11A0529 <23r11a0529@gcet.edu.in>
2025-05-23 18:18:15 +0530

    updated

--- a13/index.js
+++ a13/index.js
@@ -3,4 +3,4 @@ const products=[
     {id: 2,name:"Product 2",price:40},
     {id: 3,name:"Product 3",price:30},
 ];
-products.forEach((product) => console.log(product.name));
+products.forEach((product) => console.log(product.name));cd
